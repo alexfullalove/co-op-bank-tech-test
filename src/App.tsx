6aae6c84b97986fcd1e8f1ca858ed0376d60e8d7
alexfullalove <alexanderfullalove@gmail.com>
2022-07-24 00:14:17 +0100

    installed tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { CurrencyConverter } from "./components/currency-converter";
 const App: React.FC = (): JSX.Element => {
   return (
     <div>
-      <div className="converter-container">
+      <div className="max-w-4xl m-auto pb-6 pt-14">
         <CurrencyConverter />
       </div>
     </div>
