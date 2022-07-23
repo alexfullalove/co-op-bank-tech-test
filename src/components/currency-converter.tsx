8cb5be155c77df4e3bd351299baa9e75a9d659b3
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 23:54:15 +0100

    added converter component

---
+++ src/components/currency-converter.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CurrencyConverter: React.FC = (): JSX.Element => {
+  return (
+    <section className="">
+      <h1>Currency Converter</h1>
+    </section>
+  );
+};
